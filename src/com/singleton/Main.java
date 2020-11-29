bf1f44bc5002a4ba2c74765eea23a7e6ea6ca929
Patricia Maceiras <pmaceirasvarela@danielcastelao.org>
2020-11-29 17:10:12 +0100

    Pruebas de comprobacion

--- src/com/singleton/Main.java
+++ src/com/singleton/Main.java
@@ -5,7 +5,25 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Proyecto para aprender a hacer un Singleton");
         //Intento instanciar un objeto con el constructor privado
-        Singleton miUnicaInstancia = new Singleton();
+        /*esto es un error
+        Singleton miUnicaInstancia= new Singleton();
+        */
+
+        //Creamos un objeto
+        Singleton miUnicaInstancia= Singleton.getInstance();
+        //rellenamos los atributos
+        miUnicaInstancia.setEdad(20);
+        miUnicaInstancia.setNombre("Pepe");
+        /*
+        Creamos otro objeto Singleton , como la única manera es con getInstance(), este
+        método se encargará de devolvernos el objeto creado anteriormente
+         */
+        Singleton otraInstanciaSeraLaMisma=Singleton.getInstance();
+        /*
+        Podemos cambiar la edad , comprobar con el debug que los dos objetos
+        miUnicaInstancia y otraInstanciaSeraLaMisma son el mismo
+         */
+        otraInstanciaSeraLaMisma.setEdad(34);
     }
 
 }
