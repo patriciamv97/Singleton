eaf6c6e2ab1b94987da645ad8dc2d4d1a88a69c4
Patricia Maceiras <pmaceirasvarela@danielcastelao.org>
2020-11-29 16:43:32 +0100

    Añadimos atributos nombre y edad

--- src/com/singleton/Singleton.java
+++ src/com/singleton/Singleton.java
@@ -4,5 +4,28 @@ package com.singleton;
  * Solo una instancia
  */
 public class Singleton {
+    //Añadimos atributos para comprobar que siempre es el mismo objeto
+    private String nombre;
+    private Integer edad;
+
+    /**
+     * Constructor bloqueado con private
+     */
     private Singleton(){}
+
+    /**
+     * Metdodo que permite establecer la edad de la persona
+     * @param edad
+     */
+    public void  setEdad(Integer edad){
+        this.edad=edad;
+    }
+
+    /**
+     * Metodo que permite establecer el nombre de la persona
+     * @param nombre
+     */
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    }
 }
